9bfca7a80276b1ba728602febe5a5cee7d90eb93
Enting Zhang <e35zhang@uwaterloo.ca>
2023-03-07 10:46:38 -0500

    add defensive pylons

--- sharpy-sc2/sharpy/plans/acts/protoss/__init__.py
+++ sharpy-sc2/sharpy/plans/acts/protoss/__init__.py
@@ -5,6 +5,7 @@ from .chrono_any_tech import ChronoAnyTech
 from .chrono_tech import ChronoTech
 from .chrono_unit import ChronoUnit
 from .defensive_cannons import DefensiveCannons
+from .defensive_pylon import DefensivePylons
 from .protoss_unit import ProtossUnit
 from .restore_power import RestorePower
 from .warp_unit import WarpUnit
